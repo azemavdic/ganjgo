7e58a53646b3036712e48f029894b993cbe37c27
Azem Avdic <asko2212@hotmail.com>
2023-03-29 09:54:55 +0200

    accordions

--- pages/usluge.jsx
+++ pages/usluge.jsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React, { useState } from 'react'
 import { BsBagCheck } from 'react-icons/bs'
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
 
 const vrsteUsluge = [
   {
@@ -75,12 +76,59 @@ const buttons = [
   },
 ]
 
+const panels = [
+  {
+    id: 1,
+    title: 'Od kada postoji homologacija vozila?',
+    content:
+      'Uvođenje homologacije vozila u Bosni i Hercegovini uslijedilo je 1. januara 2011., kada je i započela primjena propisa. U ostalim evropskim zemljama ovo je uobičajeni postupak koji se provodi već desetljećima.',
+  },
+  {
+    id: 2,
+    title: 'Koja vozila zadovoljavaju kriterije homologacije?',
+    content:
+      'Za rabljena vozila svih marki okvirni pregled prolaznosti vozila opisan je u brošuri koju izdaje konzorcij Centar za automobilsko inženjerstvo d.o.o. Banja Luka, Informacioni Inžinjering Sirius 2010 d.o.o. Banja Luka i Centar za cestovni promet d.o.o. Sarajevo i na taj način ih ovlastilo za poslove homologacije vozila, dijelova uređaja i opreme vozila. Ipak, ukoliko se odlučite za uvoz vozila, a prije same kupovine, preporučujemo da kod nas kao ovlaštenog zastupnika provjerite mogućnost uvoza željenog vozila.',
+  },
+  {
+    id: 3,
+    title: 'Koja vozila podliježu provjeri homologacijske usklađenosti?',
+    content:
+      'Sva vozila koja se po prvi put registruju u Bosni i Hercegovini, uz određene izuzetke (npr. diplomatska predstavništva). Za nova vozila koja su kupljena u mreži naših ovlaštenih trgovaca, navedeni postupak obavlja ovlašteni uvoznik. U slučaju samostalnog uvoza vozila treba provesti tzv. postupak pojedinačne provjere usklađenosti vozila.',
+  },
+  {
+    id: 4,
+    title: 'Kako izvršiti provjeru prije odluke o kupovini vozila?',
+    content: '',
+  },
+  {
+    id: 5,
+    title: 'Primjer ispunjenje uplatnice',
+    content: '',
+  },
+  {
+    id: 6,
+    title: 'Vozilo je neusklađeno / Vozilo je usklađeno',
+    content:
+      'U slučaju da vozilo ne zadovoljava kriterije homologacije, preporučujemo odustajanje od kupovine, jer takvo se vozilo neće moći niti ocariniti niti registrovati u BiH; povratak u državu gdje je kupljeno također neće biti moguć ili će uzrokovati velike financijske izdatke. Ukoliko vozilo zadovoljava kriterije homologacije, nakon što obavite kupoprodajne procedure te na graničnom prijelazu prijavite uvoz vozila, možete se obratiti Vama najbližem tehničkom servisu ovlaštenom za pregled vozila, koji će online vezom zatražiti izdavanje "Potvrde proizvođača". Potvrda proizvođača je dokument koji, sa svojim tehničkim i homologacijskim podacima, služi za obavljanje homologacijskog pregleda na ovlaštenim ispitnim mjestima',
+  },
+]
+
 const Usluge = () => {
   const [activeButton, setActiveButton] = useState(1)
-  const [activePanel, setActivePanel] = useState(null)
+  const [activePanels, setActivePanels] = useState([1, 2])
 
   function handleClickButton(buttonId) {
     setActiveButton(buttonId)
+  }
+
+  function handleClickPanel(panelId) {
+    setActivePanels((prevState) => {
+      if (prevState.includes(panelId)) {
+        return prevState.filter((id) => id !== panelId)
+      } else {
+        return [...prevState, panelId]
+      }
+    })
   }
 
   return (
@@ -167,7 +215,26 @@ const Usluge = () => {
             <div className=''>
               <Image src='/images/tehnicki_pregled.jpg' alt='tehnicki_pregled' width={1500} height={1000} />
             </div>
-            <div></div>
+          </div>
+          <div className='w-full mt-10 space-y-2'>
+            {panels.map((panel) => (
+              <div key={panel.id} className=''>
+                <button
+                  className='w-full py-4 px-12 flex items-center justify-between text-xl font-bold text-left rounded-lg bg-[#2D63F6] hover:bg-[#2D63F6]/80 focus:outline-none'
+                  onClick={() => handleClickPanel(panel.id)}
+                >
+                  <span>{panel.title}</span>
+                  {activePanels.includes(panel.id) ? <FaChevronUp /> : <FaChevronDown />}
+                </button>
+                <div
+                  className={`transition-all duration-300 px-36 ease-in-out overflow-hidden text-lg ${
+                    activePanels.includes(panel.id) ? 'max-h-screen py-5' : 'max-h-0'
+                  }`}
+                >
+                  {panel.content}
+                </div>
+              </div>
+            ))}
           </div>
         </section>
       )}
